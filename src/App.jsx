a19e6cab6cf9cab7e0cf45999b77e362f208ac9e
Shashwat Gupta <shashwatsatna@gmail.com>
2022-11-26 15:10:34 +0530

    ✨ routing + tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Issues from "./Pages/Issues";
+
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path='/:org/:repo' element={<Issues />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className='grid place-items-center h-[100vh]'>
+      <div>
+        <input />
+        <Link>Go to the react issues page</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
